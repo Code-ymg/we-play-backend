0fd3f8eff57b8bad6cdb0a5656d217f6eb12ce52
Code-ymg <yashrajsinh.02.06@gmail.com>
2025-04-05 10:33:26 +0530

    Add getWatchHistory function and update user routes for watch history retrieval

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { uploadCloudianry } from "../utils/cloudinary.js";
 import { APIResponse } from "../utils/APIResponse.js";
 import jwt from "jsonwebtoken";
+import mongoose from "mongoose";
 
 const generateAccessAndRefreshToken = async(userid) => {
     try {
@@ -406,6 +407,60 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
     
 });
 
+const getWatchHistory = asyncHandler(async(req, res) =>{
+    const user = User.aggregate([
+        {
+            $match: {
+                _id: mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup: {
+                from: "videos",
+                localField: "watchHistory",
+                foreignField: "_id",
+                as: "watchHistory",
+                pipeline: [
+                    {
+                        $lookup: {
+                            from: "users",
+                            localField: "owner",
+                            foreignField: "_id",
+                            as: "owner",
+                            pipeline: [
+                                {
+                                    $project: {
+                                        fullname: 1,
+                                        username: 1,
+                                        avatar: 1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addFields: {
+                            owner: {
+                                $first: "$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+    ]);
+
+    return res
+    .status(200)
+    .json(
+        new APIResponse(
+            200,
+            user[0].watchHistory,
+            "The watch history is fetched successfully!"
+        )
+    );
+});
+
 export { 
     registerUser,
     loginUser, 
@@ -416,5 +471,6 @@ export {
     updateAccountDetails,
     updateUserAvatar,
     updateUserCoverImage,
-    getUserChannelProfile
+    getUserChannelProfile,
+    getWatchHistory
 };
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,4 +1,13 @@
-import { loginUser, logoutUser, refreshAccessToken, registerUser } from "../controllers/user.controller.js";
+import { getCurrentUser, 
+         getUserChannelProfile, 
+         getWatchHistory, 
+         loginUser, 
+         logoutUser, 
+         refreshAccessToken, 
+         registerUser, 
+         updateAccountDetails, 
+         updateCurrentPassword 
+        } from "../controllers/user.controller.js";
 import { Router } from "express";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
@@ -23,5 +32,11 @@ router.route("/login").post(loginUser);
 //* Secured routes
 router.route("/logout").post(verifyJWT, logoutUser);
 router.route("/refesh-token").post(refreshAccessToken);
-
+router.route("/update-password").post(verifyJWT, updateCurrentPassword);
+router.route("/current-user").post(verifyJWT, getCurrentUser);
+router.route("/update-user-details").patch(verifyJWT, updateAccountDetails);
+router.route("/update-user-avatar").patch(verifyJWT, upload.single("avatar"), updateAccountDetails);
+router.route("/update-user-coverImage").patch(verifyJWT, upload.single("coverImage"), updateAccountDetails);
+router.route("/channel/:username").get(verifyJWT, getUserChannelProfile);
+router.route("/watch-history").get(verifyJWT, getWatchHistory);
 export default router;
